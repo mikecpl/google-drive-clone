d9eef056130911ad57907ec6c918bb1a4e06ae68
mikecpl <mikecorporal@gmail.com>
2022-08-25 22:14:57 +0200

    Added navbar component.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import useAuth from '../hooks/useAuth';
+import NavBar from './NavBar';
 
 export default function Dashboard() {
-  const { user, logout } = useAuth();
-  const navigate = useNavigate();
-
-  async function handleLogout() {
-    await logout();
-    navigate('/login');
-  }
-
   return (
-    <>
-      <div>{user.email}</div>
-      <button onClick={handleLogout}>
-        Kijelentkezés
-      </button>
-    </>
+    <NavBar />
   )
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+export default function NavBar() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    await logout();
+    navigate('/login');
+  }
+
+  return (
+    <nav className="bg-white border border-gray-200 p-4">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link to="/" className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap text-blue-600">Google Drive</span>
+        </Link>
+        <div className="w-auto" id="navbar-default">
+          <button onClick={handleLogout} className="hover:text-blue-600">
+            Kijelentkezés
+          </button>
+        </div>
+      </div>
+    </nav>
+  )
+}
