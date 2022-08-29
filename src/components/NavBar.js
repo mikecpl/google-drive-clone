e7c86ecce509e0bd96e2aaf8b9ddb197cc390d3e
mikecpl <mikecorporal@gmail.com>
2022-08-30 00:59:01 +0200

    CS fix.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,7 +15,9 @@ export default function NavBar() {
     <nav className="bg-white border border-gray-200 p-4">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link to="/" className="flex items-center">
-          <span className="self-center text-xl font-semibold whitespace-nowrap text-blue-600">Google Drive Clone</span>
+          <span className="self-center text-xl font-semibold whitespace-nowrap text-blue-600">
+            Google Drive Clone
+          </span>
         </Link>
         <div className="w-auto" id="navbar-default">
           <button onClick={handleLogout} className="text-gray-500 hover:text-blue-600">
