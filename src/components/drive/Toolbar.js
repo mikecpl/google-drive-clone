6b30dbb023e67267f3f271635ff0ef99928e8800
mikecpl <mikecorporal@gmail.com>
2022-08-29 02:04:40 +0200

    Fixed the onclicks.

--- src/components/drive/Toolbar.js
+++ src/components/drive/Toolbar.js
@@ -16,12 +16,16 @@ export default function Toolbar({ currentFolder }) {
 
   return (
     <div className="flex flex-row space-x-2">
-      <button className="bg-white border text-blue-600 hover:text-blue-800 border-blue-600 hover:border-blue-800 rounded-md py-2 px-3">
-        <FontAwesomeIcon icon={faUpload} onClick={openUploadFileModal} />
+      <button className="bg-white border text-blue-600 hover:text-blue-800 border-blue-600 hover:border-blue-800 rounded-md py-2 px-3"
+        onClick={openUploadFileModal}
+      >
+        <FontAwesomeIcon icon={faUpload} />
       </button>
 
-      <button className="bg-white border text-blue-600 hover:text-blue-800 border-blue-600 hover:border-blue-800 rounded-md py-2 px-3">
-        <FontAwesomeIcon icon={faFolderPlus} onClick={openAddFolderModal} />
+      <button className="bg-white border text-blue-600 hover:text-blue-800 border-blue-600 hover:border-blue-800 rounded-md py-2 px-3"
+        onClick={openAddFolderModal}
+      >
+        <FontAwesomeIcon icon={faFolderPlus} />
       </button>
 
       <AddFolderModal open={isAddFolderModalOpen}
